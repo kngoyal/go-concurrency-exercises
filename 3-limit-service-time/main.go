ddf16a501226b429af74221863cb00a2c90cd743
supatomic <kngoyal.cs@gmail.com>
2022-01-14 12:36:54 -0500

    limit service time per request

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,19 +10,98 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
-	ID        int
-	IsPremium bool
-	TimeUsed  int64 // in seconds
+	ID                 int
+	IsPremium          bool
+	TimeUsed           int64 // in seconds
+	ConcurrentRequests int
+	sync.Mutex
+}
+
+func HandleRequestByRequestLimit(process func(), u *User) bool {
+	if u.IsPremium {
+		process()
+		return true
+	}
+	c := make(chan bool)
+
+	go func() {
+		process()
+		c <- true
+	}()
+
+	select {
+	case <-c:
+		return true
+	case <-time.After(10 * time.Second):
+		return false
+	}
+}
+
+func (u *User) GetTimeUsed() int64 {
+	u.Lock()
+	defer u.Unlock()
+	return u.TimeUsed
+}
+
+func (u *User) SetTimeUsed(t int64) {
+	u.Lock()
+	defer u.Unlock()
+	u.TimeUsed += t
+}
+
+func (u *User) UpdateConcurrentRequests(t int) {
+	u.Lock()
+	defer u.Unlock()
+	u.ConcurrentRequests += t
+}
+
+func (u *User) GetConcurrentRequests() int {
+	u.Lock()
+	defer u.Unlock()
+	return u.ConcurrentRequests
+	//return int(math.Max(1.0, float64(u.ConcurrentRequests)))
+}
+
+func HandleRequestByUserLimit(process func(), u *User) bool {
+	if u.IsPremium {
+		process()
+		return true
+	}
+	c := make(chan time.Time)
+	start := time.Now()
+	go func() {
+		u.UpdateConcurrentRequests(1)
+		process()
+		u.UpdateConcurrentRequests(-1)
+		c <- time.Now()
+	}()
+
+	select {
+	case t1 := <-c:
+		fmt.Println("completed", start, t1)
+		u.SetTimeUsed(int64(t1.Sub(start).Seconds()))
+		return true
+	case t2 := <-time.After(time.Duration(10-u.GetTimeUsed()) / time.Duration(1+u.GetConcurrentRequests()) * time.Second):
+		fmt.Println("killed", start, t2)
+		u.SetTimeUsed(int64(t2.Sub(start).Seconds()))
+		return false
+	}
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	return HandleRequestByUserLimit(process, u)
+	//return HandleRequestByRequestLimit(process, u)
 }
 
 func main() {
